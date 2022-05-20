e8815ad5335ceaaaacfa62e4871ae01d31ff98a5
Rubel Talukder <talukderrubel223@gmail.com>
2022-05-20 23:16:49 +0600

    jsonToken added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 const { query } = require('express');
+var jwt = require('jsonwebtoken');
 const app = express()
 require('dotenv').config()
 const port = process.env.PORT || 5000
@@ -38,7 +39,8 @@ async function run() {
         $set: user,
       };
       const result = await userCollection.updateOne(filter, updateDoc, options);
-      res.send(result);
+      const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+      res.send({ result, token });
     })
 
     app.get('/available', async (req, res) => {
